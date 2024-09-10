5431f729468ec561e35278dd485f019e8ae17eb7
Roland-Sankara <roland1sankara@gmail.com>
2024-09-10 21:38:33 +0300

    create CI workflow

--- __tests__/api-test.js
+++ __tests__/api-test.js
@@ -1,8 +1,20 @@
 import request from "supertest";
 import app from "../server.js";
-import {expect, test} from "@jest/globals";
+import {expect, test, beforeAll} from "@jest/globals";
+// import {PrismaClient} from "@prisma/client"
+
+beforeAll(async()=>{
+    let response = await request(app).post('/api/v1/users').send({
+        "username": "Roland-One",
+        "password": "34464646",
+        "email": "one@email.com"
+    }).set('Accept', 'application/json')
+
+    expect(response.status).toBe(201)
+})
+
 
 test('get array of users from the user endpoint', async function(){
-    let response = await request(app).get('/api/v1/users/?results=3')
+    let response = await request(app).get('/api/v1/users/?results=5')
     expect(response.status).toBe(200)
 })
