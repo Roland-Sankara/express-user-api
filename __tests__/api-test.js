92e7ff2abfa34e4b8ada3122676e4182f0f102bf
Roland-Sankara <roland1sankara@gmail.com>
2024-09-11 08:47:02 +0300

    update the tests

--- __tests__/api-test.js
+++ __tests__/api-test.js
@@ -1,7 +1,8 @@
 import request from "supertest";
 import app from "../server.js";
-import {expect, test, beforeAll} from "@jest/globals";
+import {expect, test, beforeAll, afterAll} from "@jest/globals";
 // import {PrismaClient} from "@prisma/client"
+// const prisma = new PrismaClient();
 
 beforeAll(async()=>{
     let response = await request(app).post('/api/v1/users').send({
@@ -12,6 +13,17 @@ beforeAll(async()=>{
 
     expect(response.status).toBe(201)
 })
+
+// afterAll(async()=>{
+//     let user = await prisma.
+//     let response = await request(app).delete('/api/v1/users').send({
+//         "username": "Roland-One",
+//         "password": "34464646",
+//         "email": "one@email.com"
+//     }).set('Accept', 'application/json')
+
+//     expect(response.status).toBe(201)
+// })
 
 
 test('get array of users from the user endpoint', async function(){
